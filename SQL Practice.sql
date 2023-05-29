7bb6cd269803ab9380b22cf403b33a20a349c622
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-05-29 13:37:46 -0400

    Solved two more problems

--- SQL Practice.sql
+++ SQL Practice.sql
@@ -145,3 +145,76 @@ FROM
     JOIN Prices ON Prices.product_id = UnitsSold.product_id
         AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
 GROUP BY UnitsSold.product_id
+
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key of this table.
+product_id is a foreign key to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write an SQL query that reports the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+
+The query result format is in the following example.
+*/
+
+SELECT
+    product_name, year, price
+FROM 
+    Sales
+LEFT JOIN
+    Product ON Sales.product_id = Product.product_id
+
+
+/*
+Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key for this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+Write an SQL query to report the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+The query result format is shown in the following example.
+*/
+
+SELECT
+    teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM
+    Teacher
+GROUP BY 
+    teacher_id
