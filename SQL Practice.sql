e8d0f8145d5d79290245cb0c5df1970456a50bfe
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-06-12 17:01:06 -0400

    Added 1 more

--- SQL Practice.sql
+++ SQL Practice.sql
@@ -398,3 +398,13 @@ Query the list of CITY names from STATION that either
 SELECT DISTINCT City
 FROM Station
 WHERE City NOT LIKE '%[AEIOU]' OR City NOT LIKE '[AEIOU]%';
+
+/*
+Query the list of CITY names from STATION that 
+do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT City
+FROM Station 
+WHERE City NOT LIKE '%[AEIOU]' AND City NOT LIKE '[AEIOOU]%';
