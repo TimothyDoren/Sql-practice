9e971991944a58d4c9246c9220595ecb0d12b26b
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-06-15 16:09:45 -0400

    One more

--- SQL Practice.sql
+++ SQL Practice.sql
@@ -420,3 +420,20 @@ SELECT Name
 FROM Students
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID ASC;
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) 
+for employees in Employee having a salary greater than 2000 per
+month who have been employees for less than  months. Sort your result by ascending employee_id.
+*/
+
+SELECT Name
+FROM Employee
+WHERE salary > 2000 
+    AND months < 10
+ORDER BY employee_id ASC;
+
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+
